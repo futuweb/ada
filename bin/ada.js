76e8b4243ba138d2b9cd9ffd2cc2d9c08d5f560b
frehkxu <frehkxu@futunn.com>
2020-01-17 12:43:20 +0800

    add stage param #1

--- bin/ada.js
+++ bin/ada.js
@@ -10,6 +10,7 @@ const token = args.token || args.t;
 const commitId = args.commit || args.c;
 const apiVersion = args.apiver || args.a;
 const filePath = path.resolve(args.output || args.o || 'artifacts.zip');
+const stageName = args.stage || args.s;
 
 const help = `
 Ada: donwload artifacts of Gitlab CI pipelines/jobs.
@@ -18,18 +19,19 @@ Arguments:
 
     --gitlab    -g     Gitlab http(s) url
     --project   -p     Project id
+    --stage     -s     Which CI stage download Zip (default: first stage)  
     --token     -t     Gitlab private token
     --commit    -c     Commit id
     --output    -o     Output file (default: ./artifacts.zip)
     --apiver    -a     API Version (default: 4)
-`
+`;
 
 if(!projectId || !token || !commitId){
     console.log(help);
     process.exit(1);
 }
 
-const ada = new Ada(gitlabUrl, projectId, token, apiVersion);
+const ada = new Ada(gitlabUrl, projectId, token, apiVersion, stageName);
 console.log('ready to download.');
 ada.download(commitId, filePath).then(() => {
     console.log('artifacts saved: ' + filePath);
